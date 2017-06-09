698755ea8a61742b176ea7821747102c58edad4f
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-06-09 13:03:23 -0400

    adding film selection by year

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 function getFilmRecommendations(req, res) {
   db.Films.getAll({
 
-    where {
+    where: {
       id: req.params.id
     }
 
@@ -28,6 +28,20 @@ function getFilmRecommendations(req, res) {
     fifteenYearsBefore = (releaseYear - 15),
     fifteenYearsAfter = (releaseYear + 15);
     let recommendFilms = [];
+
+    db.Films.findAll({
+      where: {
+        genre_id: genreId
+      }
+    }).then(function(selectedFilms){
+      for(var i = 0, i < selectedFilms, i++){
+        let grabYear = parseInt(selectedFilms[i].release_date.substring(0, 4));
+        if (grabYear <= fifteenYearsAfter && grabYear >= fifteenYearsBefore) {
+          recommendFilms.push(selectedFilms[i]);
+        }
+      }
+      res.json({'recommendations': recommendFilms});
+    })
   })
   res.status(500).send('Not Implemented');
 }
